42e9d35dde0785dbd77fb9489db784a5d676be7f
Sruthik Koneti <sruthikkoneti@gmail.com>
2024-01-05 22:02:12 +0530

    Update weather.js

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -1,20 +1,24 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getWeather = async (req, res) => {
-    const { cities } = req.body;
-    console.log(cities)
-    const weatherData = {};
-    const apiKey = process.env.WEATHER_API_KEY
-    try {
-        const promises = cities.map(async (city) => {
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-            const temperature = response.data.main.temp;
-            weatherData[city] = `${temperature}C`;
-        });
+  const { cities } = req.body;
+  const weatherData = {};
+  const apiKey = process.env.WEATHER_API_KEY;
 
-        await Promise.all(promises);
-        return res.status(200).json({ weather: weatherData });
-    } catch (err) {
-        return res.status(400).json(err.message)
-    }
-}
+  try {
+    const promises = cities.map(async (city) => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+        const temperature = response.data.main.temp;
+        weatherData[city] = `${temperature}C`;
+      } catch (error) {
+        weatherData[city] = 'Not available';
+      }
+    });
+
+    await Promise.all(promises);
+    return res.status(200).json({ weather: weatherData });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
