c338075a1d21f2a7ba330fa24178a0a21daeba26
Sruthik Koneti <sruthikkoneti@gmail.com>
2024-01-05 20:41:54 +0530

    Update weather.js

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -8,13 +8,11 @@ export const getWeather=async(req,res)=>{
         try {
           const promises = cities.map(async (city) => {
             const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-            console.log(response.data)
             const temperature = response.data.main.temp;
             weatherData[city] = `${temperature}C`;
           });
       
           await Promise.all(promises);
-        //   return res.json({msg:"success"})
           return res.status(200).json({ weather: weatherData });
     }catch(err){
         return res.status(400).json(err.message)
